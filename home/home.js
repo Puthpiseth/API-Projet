b3cf1fbd6cd1971638d6fe0a69883a36eaa5a8e2
myhobbies889 <myhobbies123@hotmail.com>
2021-02-13 12:25:01 -0500

    home.js update

--- home/home.js
+++ home/home.js
@@ -1,20 +1,122 @@
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-const liens = document.querySelectorAll('.menu a');
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const liens = document.querySelectorAll(".menu a");
 
-burger.addEventListener('click', () => {
-    menu.classList.toggle('menu-open');
-    burger.classList.toggle('burger-cross');
+burger.addEventListener("click", () => {
+  menu.classList.toggle("menu-open");
+  burger.classList.toggle("burger-cross");
 });
 
 liens.forEach((lien) => {
-    lien.addEventListener('click', () => {
-        menu.classList.remove('menu-open');
-        burger.classList.remove('burger-cross');
-    });
+  lien.addEventListener("click", () => {
+    menu.classList.remove("menu-open");
+    burger.classList.remove("burger-cross");
+  });
 });
 
 
 
+function getArticles() {
+  let token = localStorage.getItem("token");
+  console.log(token);
+  let dataRequest = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  };
+  fetch("https://simplonews.brianboudrioux.fr/articles", dataRequest)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      generateArticles(data);
+    });
+}
+getArticles();
+function generateArticles(data) {
+  //   console.log(data.articles);
+  let articleDiv = document.querySelector(".text");
+  let articlesSimplon = "";
+  let articleDiv2 = document.querySelector("section > div > div");
+  let articlesSimplon2 = "";
+  let slideShow = document.querySelector(".slider");
+  let slideShowElem = "";
+  for (let i = 0; i < data.articles.length; i++) {
+    let id = data.articles[0].id;
+    let titre = data.articles[0].title;
+    let image = data.articles[0].img;
+    let id1 = data.articles[1].id;
+    let titre1 = data.articles[1].title;
+    let image1 = data.articles[1].img;
+    let id2 = data.articles[2].id;
+    let titre2 = data.articles[2].title;
+    let image2 = data.articles[2].img;
+    let id7 = data.articles[7].id;
+    let titre7 = data.articles[7].title;
+    let image7 = data.articles[7].img;
+    let id4 = data.articles[4].id;
+    let titre4 = data.articles[4].title;
+    let image4 = data.articles[4].img;
+    let id10 = data.articles[10].id;
+    let titre10 = data.articles[10].title;
+    let image10 = data.articles[10].img;
+    let id15 = data.articles[15].id;
+    let titre15 = data.articles[15].title;
+    let image15 = data.articles[15].img;
+
+    articlesSimplon = `<div class="text PremArticles" id="articl2">
+    <p>${titre1}.</p>
+    <a href="">
+        <button>lire plus</button>
+    </a>
+</div>`;
+    articlesSimplon2 = `<div class="article3  text" id="article3" >
+<p>${titre15}</p>
+<a href="">
+    <button>lire plus</button>
+</a>
+</div>`;
+    slideShowElem = `<div class="slides">
+<div class="slide"><p>${titre2} <a href=""><button>lire plus</button></a></p><img src="${image2}" alt="sweeney todd"></div>
+<div class="slide"><p>${titre7}<a href=""><button>lire plus</button></a></p><img src="${image7}" alt="Bellatrix"></div>
+<div class="slide"><p>${titre4}<a href=""><button>lire plus</button></a></p><img src="${image4}" alt="queen of hearts"></div>
+<div class="slide"><p>${titre10}</p><img src="${image10}" alt="dracula"></div>
+</div>`;
+  }
+  articleDiv.insertAdjacentHTML("afterend", articlesSimplon);
+  slideShow.insertAdjacentHTML("afterbegin", slideShowElem);
+  articleDiv2.insertAdjacentHTML("afterend", articlesSimplon2);
+  
+};
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+// function redirection(id){
+//     location.href= "http://127.0.0.1:5500/ArticlePage/Article.html?id="+id
+// }
+// getArticles();
+// function  generateArticles(data){
+//     console.log(data.articles);
+//     let divArticle = document.querySelector('.text');
+//     let articles = "";
+//     for(let i =0; i<data.articles.length; i++){
+//         let id = data.articles[i].id;
+//         let titre = data.articles[i].title;
+//         let image = data.articles[i].img;
+//         let resume = data.articles[i].resume;
+//         let author = data.articles[i].author;
+
+//         articles += ` <div class="text PremArticles" id="articl1" onclick="redirection(${id})">
+//         <h1>${titre}</h1>
+//         <h2>${author}</h2>
+//         <img src="${image}" alt="">
+//       <p>${resume}.</p>
+//     </div>`
+//     }
+//     divArticle.innerHTML = articles;
+// }
+// function redirection(id){
+//     location.href= "http://127.0.0.1:5500/ArticlePage/Article.html?id="+id
+// }
--- login.js
+++ login.js
@@ -4,56 +4,48 @@ const loginBtn = document.querySelector("#login");
 const registerBtn = document.querySelector("#register");
 const btn = document.querySelector("#btn");
 
-
 function register() {
-    loginBtn.style.left = "-400px";
-    registerBtn.style.left = "50px";
-    btn.style.left = "110px";
+  loginBtn.style.left = "-400px";
+  registerBtn.style.left = "50px";
+  btn.style.left = "110px";
 }
 
 function login() {
-    loginBtn.style.left = "50px";
-    registerBtn.style.left = "450px";
-    btn.style.left = "0";
+  loginBtn.style.left = "50px";
+  registerBtn.style.left = "450px";
+  btn.style.left = "0";
 }
 
 // Affiche un message-error pour username et password
 
-const emailInput = document.querySelector("#email").value;
-const pswInput = document.querySelector("#psw").value;
-const userError = document.querySelector(".usernameError");
-const pswError = document.querySelector(".passwordError");
+// const emailInput = document.querySelector("#email").value;
+// const pswInput = document.querySelector("#psw").value;
+// const userError = document.querySelector(".usernameError");
+// const pswError = document.querySelector(".passwordError");
 
-document.querySelector("#login-btn").addEventListener("click", () => {
-   
-    const emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}";
-    if (emailInput !== emailRegex || emailInput === "" || emailInput == null && pswInput.length < 8) {
-        userError.style.display = "block";
-        pswError.style.display = "block";
+function getTocken() {
+  let dataRequest = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: "test@test.fr", password: "test1234" }),
+  };
+  const url = "https://simplonews.brianboudrioux.fr/users/login";
+  const urlResponse = fetch(url, dataRequest).then(function (response) {
+    if (response.status == 400) {
+      console.log("data request  error");
+    } else {
+      response.json().then(function (data) {
+        console.log(data);
+        let token = data.token;
+        localStorage.setItem("token", token);
+        window.location.href = "http://127.0.0.1:5501/home/home.html";
+      });
     }
-
-    if(emailInput === emailRegex && pswInput.length > 8) {
-        userError.style.display = "none";
-        pswError.style.display = "none";
-    }
-
-});
+  });
+}
+getTocken();
 
 
 
-
-
-
-
-
-
-
-// redirection la page home
-
-// https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
-// https://www.youtube.com/watch?v=Swzem9EdFhw
-
-
-// document.querySelector(".submit-btn").addEventListener("click", function() {
-//     window.location.href = "http://127.0.0.1:5501/home.html."
-// });
